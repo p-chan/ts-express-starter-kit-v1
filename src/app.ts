c6fd50cdd444d7f4889adfaaab211373aeb9f10f
P-Chan <hello@0x50.io>
2019-05-22 22:32:08 +0900

    feat: add logger

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+import morgan from 'morgan'
 import router from './router'
 import errorHandler from './middlewares/errorHandler'
 import notFound from './middlewares/notFound'
 
 const app = express()
+
+app.use(morgan('dev'))
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
