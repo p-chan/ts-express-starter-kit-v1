bf49e692a20cf24e756ccd8943db49c8fddf952f
P-Chan <hello@0x50.io>
2019-05-23 11:48:15 +0900

    feat: add helmet

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import morgan from 'morgan'
+import helmet from 'helmet'
 import router from './router'
 import errorHandler from './middlewares/errorHandler'
 import notFound from './middlewares/notFound'
 
 const app = express()
+
+app.use(helmet())
 
 app.use(morgan('dev'))
 
