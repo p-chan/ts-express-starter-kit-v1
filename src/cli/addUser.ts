49e2252ed45b911da8f5b693c8d9de588d673c28
P-Chan <hello@0x50.io>
2019-05-25 19:27:15 +0900

    feat: add user seed script

---
+++ src/cli/addUser.ts
@@ -0,0 +1,33 @@
+import { createConnection, getRepository } from 'typeorm'
+import { validate } from 'class-validator'
+import { cli } from 'cli-ux'
+import { User } from '../db/entities/User'
+
+createConnection()
+  .then(async connection => {
+    let user = new User()
+    user.name = await cli.prompt('What is your name?')
+    user.screenName = await cli.prompt('What is your screen name')
+    user.email = await cli.prompt('What is your email')
+    await user.hashPassword(
+      await cli.prompt('What is your password?', { type: 'hide' })
+    )
+
+    await cli.confirm('Continue?')
+
+    await validate(user).then(async errors => {
+      if (errors.length) {
+        console.error(errors)
+        throw new Error('Error')
+      }
+    })
+
+    user = await getRepository(User).save(user)
+
+    console.log(user)
+
+    process.exit(0)
+  })
+  .catch(error => {
+    console.error(error)
+  })
